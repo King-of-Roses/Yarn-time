3f8477b1364f25ca54799d41c73201b24daab639
irmelij <irmelij@student.chalmers.se>
2025-02-28 17:05:27 +0100

    camera target group, platforms level 2, door transfer

---
+++ My project/Assets/Door.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    private int players_touching;
+    
+    private void Start()
+    {
+        players_touching = 0;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            players_touching++;
+            Check_players_touching();
+        }
+    }
+
+    private void OnTriggerExit2D (Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            players_touching--;
+        }
+    }
+
+    private void Check_players_touching ()
+    {
+        if (players_touching == 2) 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
