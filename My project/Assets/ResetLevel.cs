96aa950ccafa8905a971fcde2ae377480d28c395
douglas <gusjakdo@student.gu.se>
2025-03-03 23:51:46 +0100

    Made spikes into a tilemap
    Spikes groups are now a tilemap instead of individual gameobjects.

--- My project/Assets/ResetLevel.cs
+++ My project/Assets/ResetLevel.cs
@@ -4,19 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Trap : MonoBehaviour
+public class ResetLevel : MonoBehaviour
 {
-    public float bounceForce = 10f;
-    public int damage = 1;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-
-
         }
     }
 }
--- My project/Assets/Switch1.cs
+++ My project/Assets/Switch1.cs
@@ -1,44 +1,36 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Switch : MonoBehaviour
 {
     public GameObject trap;
-    public GameObject trap1;
-    public GameObject trap2;
-    public GameObject trap3;
-    public GameObject trap4;
-    public GameObject trap5;
-    public GameObject trap6;
+    
+    // Counts the number of activations, max is the number of players
+    private int switchCounter = 0;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player")) {}
+        if (collision.gameObject.CompareTag("Player"))
         {
-            //gameObject.SetActive();
+            switchCounter++;
             trap.gameObject.SetActive(false);
-            trap1.gameObject.SetActive(false);
-            trap2.gameObject.SetActive(false);
-            trap3.gameObject.SetActive(false);
-            trap4.gameObject.SetActive(false);
-            trap5.gameObject.SetActive(false);
-            trap6.gameObject.SetActive(false);
         }
     }
-
+    
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player")) { }
+        if (collision.gameObject.CompareTag("Player") && switchCounter < 2)
         {
-            //gameObject.SetActive(true);
+            switchCounter--;
             trap.gameObject.SetActive(true);
-            trap1.gameObject.SetActive(true);
-            trap2.gameObject.SetActive(true);
-            trap3.gameObject.SetActive(true);
-            trap4.gameObject.SetActive(true);
-            trap5.gameObject.SetActive(true);
-            trap6.gameObject.SetActive(true);
+        }
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            switchCounter--;
         }
     }
 }
