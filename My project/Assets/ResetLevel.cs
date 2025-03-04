d1545cb7ced8544d292d69ce8ca1050fbe49b7bb
irmelij <irmelij@student.chalmers.se>
2025-03-04 01:19:20 +0100

    new sprites door, spikes and bouncyBlock

--- My project/Assets/ResetLevel.cs
+++ My project/Assets/ResetLevel.cs
@@ -4,17 +4,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class Trap : MonoBehaviour
 {
     public float bounceForce = 10f;
     public int damage = 1;
+    public PlayerController playerController;
+    public PlayerController2 playerController2;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            playerController.Die();
+            playerController2.Die();
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 
 
         }
--- My project/Assets/Switch1.cs
+++ My project/Assets/Switch1.cs
@@ -9,12 +9,7 @@ public class Switch : MonoBehaviour
     public Sprite button_pressed;
     public SpriteRenderer switchRenderer;
     public GameObject trap;
-    public GameObject trap1;
-    public GameObject trap2;
-    public GameObject trap3;
-    public GameObject trap4;
-    public GameObject trap5;
-    public GameObject trap6;
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -23,12 +18,7 @@ public class Switch : MonoBehaviour
             switchRenderer.sprite = button_pressed;
 
             trap.gameObject.SetActive(false);
-            trap1.gameObject.SetActive(false);
-            trap2.gameObject.SetActive(false);
-            trap3.gameObject.SetActive(false);
-            trap4.gameObject.SetActive(false);
-            trap5.gameObject.SetActive(false);
-            trap6.gameObject.SetActive(false);
+            
         }
     }
 
@@ -40,12 +30,6 @@ public class Switch : MonoBehaviour
             switchRenderer.sprite = button_standard;
 
             trap.gameObject.SetActive(true);
-            trap1.gameObject.SetActive(true);
-            trap2.gameObject.SetActive(true);
-            trap3.gameObject.SetActive(true);
-            trap4.gameObject.SetActive(true);
-            trap5.gameObject.SetActive(true);
-            trap6.gameObject.SetActive(true);
         }
     }
 }
