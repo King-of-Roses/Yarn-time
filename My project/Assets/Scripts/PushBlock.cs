7c99b43b17f49426517fb67aeb22510ad2fa46fd
irmelij <irmelij@student.chalmers.se>
2025-03-10 22:02:37 +0100

    Updates

--- My project/Assets/Scripts/PushBlock.cs
+++ My project/Assets/Scripts/PushBlock.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Friction : MonoBehaviour
+public class PushBlock : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float x;
@@ -15,13 +15,17 @@ public class Friction : MonoBehaviour
         {
             Vector2 force = collision.gameObject.GetComponent<Rigidbody2D>().velocity * new Vector2(x, 0);
            
-            if (force.x < 6f && force.x > -6f)
-            {
-                rb.velocity = force;
-            }
-            else
+            if (force.x > 6f)
             {
                 rb.velocity = new Vector2(6f, 0);
+            }
+            else if (force.x < -6f)
+            {
+                rb.velocity = new Vector2(-6f, 0);
+            } 
+            else
+            {
+                rb.velocity = force;    
             }
             
         }
